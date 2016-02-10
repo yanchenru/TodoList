a392a127276ed37db6f1ccfff646ce788c6deb81
Chenru Yan <yanchenru@gmail.com>
2016-02-10 16:38:36 -0700

    changes

--- simple-todos.js
+++ simple-todos.js
@@ -1,16 +1,75 @@
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
     // This code only runs on the client
     Template.body.helpers({
-        tasks: [
-            {
-                text: "This is task 1"
-            },
-            {
-                text: "This is task 2"
-            },
-            {
-                text: "This is task 3"
+        tasks: function () {
+            if (Session.get("hideCompleted")) {
+                // If hide completed is checked, filter tasks
+                return Tasks.find({
+                    checked: {
+                        $ne: true
+                    }
+                }, {
+                    sort: {
+                        createdAt: -1
+                    }
+                });
+            } else {
+                // Otherwise, return all of the tasks
+                return Tasks.find({}, {
+                    sort: {
+                        createdAt: -1
+                    }
+                });
             }
-    ]
+        },
+        hideCompleted: function () {
+            return Session.get("hideCompleted");
+        },
+        incompleteCount: function () {
+            return Tasks.find({
+                checked: {
+                    $ne: true
+                }
+            }).count();
+        }
+    });
+
+    Template.body.events({
+        "submit .new-task": function (event) {
+            // Prevent default browser form submit
+            event.preventDefault();
+
+            // Get value from form element
+            var text = event.target.text.value;
+
+            // Insert a task into the collection
+            Tasks.insert({
+                text: text,
+                createdAt: new Date() // current time
+            });
+
+            // Clear form
+            event.target.text.value = "";
+        },
+
+        "change .hide-completed input": function (event) {
+            Session.set("hideCompleted", event.target.checked);
+        }
+    });
+
+    Template.task.events({
+        "click .toggle-checked": function () {
+            // Set the checked property to the opposite of its current value
+            Tasks.update(this._id, {
+                $set: {
+                    checked: !this.checked
+                }
+            });
+        },
+        "click .delete": function () {
+            Tasks.remove(this._id);
+        }
     });
 }
